b1f810e309d3895bca25ad4f6d720e65ad0bc3e5
Andres Kouvach <akouvach@yahoo.com>
2020-08-19 22:07:47 -0300

    simple

--- server.js
+++ server.js
@@ -61,233 +61,233 @@ DELETE: Se utilizan para enviar datos destinados a eliminar
 //Esta es una petición de enviar datos
 // y esta direccionada a la ruta "/insertar"
 
-// esta es una petición de borrado
-app.delete("/borrarTodo", (req, res, next) => {
-  var instruccion = "delete * from usuarios";
+// // esta es una petición de borrado
+// app.delete("/borrarTodo", (req, res, next) => {
+//   var instruccion = "delete * from usuarios";
 
-  //Voy a insertar lo que me mandaron por parámetro
-  connection
-    .execute(instruccion)
-    .then((data) => {
-      console.log(JSON.stringify(data, null, 2));
-    })
-    .catch((error) => {
-      res.render(error);
-    });
+//   //Voy a insertar lo que me mandaron por parámetro
+//   connection
+//     .execute(instruccion)
+//     .then((data) => {
+//       console.log(JSON.stringify(data, null, 2));
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
 
-  res.json({ mensaje: "borrado exitoso" });
-});
+//   res.json({ mensaje: "borrado exitoso" });
+// });
 
-app.post("/insertar", (req, res, next) => {
-  var registro = req.body;
-  var instruccion =
-    "Insert into usuarios (nombre, apellido, email, pais) values ('" +
-    registro.nombre +
-    "','" +
-    registro.apellido +
-    "','" +
-    registro.email +
-    "','" +
-    registro.pais +
-    "')";
+// app.post("/insertar", (req, res, next) => {
+//   var registro = req.body;
+//   var instruccion =
+//     "Insert into usuarios (nombre, apellido, email, pais) values ('" +
+//     registro.nombre +
+//     "','" +
+//     registro.apellido +
+//     "','" +
+//     registro.email +
+//     "','" +
+//     registro.pais +
+//     "')";
 
-  //Voy a insertar lo que me mandaron por parámetro
-  connection
-    .execute(instruccion)
-    .then((data) => {
-      console.log(JSON.stringify(data, null, 2));
-    })
-    .catch((error) => {
-      res.render(error);
-    });
+//   //Voy a insertar lo que me mandaron por parámetro
+//   connection
+//     .execute(instruccion)
+//     .then((data) => {
+//       console.log(JSON.stringify(data, null, 2));
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
 
-  res.json({ mensaje: "insercion exitoas" });
-});
+//   res.json({ mensaje: "insercion exitoas" });
+// });
 
-//fin de ejemplos
+// //fin de ejemplos
 
-app.post("/crearusuarios", (req, res, next) => {
-  var registro = req.body;
-  var instruccion =
-    "Insert into usuarios (empresa, Nombre_usuario, contra, Nivel) values (" +
-    registro.empresa +
-    ",'" +
-    registro.usuario +
-    "','" +
-    registro.contrasena +
-    "'," +
-    registro.nivel +
-    ")";
-  console.log(instruccion);
-  //Voy a insertar lo que me mandaron por parámetro
-  connection
-    .execute(instruccion)
-    .then((data) => {
-      console.log(JSON.stringify(data, null, 2));
-      res.json({ mensaje: "insercion exitosa" });
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+// app.post("/crearusuarios", (req, res, next) => {
+//   var registro = req.body;
+//   var instruccion =
+//     "Insert into usuarios (empresa, Nombre_usuario, contra, Nivel) values (" +
+//     registro.empresa +
+//     ",'" +
+//     registro.usuario +
+//     "','" +
+//     registro.contrasena +
+//     "'," +
+//     registro.nivel +
+//     ")";
+//   console.log(instruccion);
+//   //Voy a insertar lo que me mandaron por parámetro
+//   connection
+//     .execute(instruccion)
+//     .then((data) => {
+//       console.log(JSON.stringify(data, null, 2));
+//       res.json({ mensaje: "insercion exitosa" });
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.get("/usuarios", (req, res, next) => {
-  connection
-    .query("SELECT * FROM usuarios")
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+// app.get("/usuarios", (req, res, next) => {
+//   connection
+//     .query("SELECT * FROM usuarios")
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.get("/Empresa", (req, res, next) => {
-  connection
-    .query("SELECT * FROM Empresa")
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+// app.get("/Empresa", (req, res, next) => {
+//   connection
+//     .query("SELECT * FROM Empresa")
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.get("/Stock/:Nombre_usuario", (req, res, next) => {
-  connection
-    .query(
-      // "SELECT * FROM Stock"
+// app.get("/Stock/:Nombre_usuario", (req, res, next) => {
+//   connection
+//     .query(
+//       // "SELECT * FROM Stock"
 
-      "SELECT * FROM Stockcompleto where Nombre_usuario ='" +
-        req.params.Nombre_usuario +
-        "'"
-    )
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+//       "SELECT * FROM Stockcompleto where Nombre_usuario ='" +
+//         req.params.Nombre_usuario +
+//         "'"
+//     )
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.get("/usuarios/:nombre_usuario", (req, res, next) => {
-  connection
-    .query(
-      "SELECT * FROM Usuarios where nombre_usuario= '" +
-        req.params.nombre_usuario +
-        "'"
-    )
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+// app.get("/usuarios/:nombre_usuario", (req, res, next) => {
+//   connection
+//     .query(
+//       "SELECT * FROM Usuarios where nombre_usuario= '" +
+//         req.params.nombre_usuario +
+//         "'"
+//     )
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.post("/crearempresa", (req, res, next) => {
-  var registro = req.body;
-  var instrucciona =
-    "Insert into Empresa (Empresa) values ('" + registro.empresax + "')";
-  console.log(instrucciona);
-  //Voy a insertar lo que me mandaron por parámetro
-  connection
-    .execute(instrucciona)
-    .then((data) => {
-      console.log(JSON.stringify(data, null, 2));
-      res.json({ mensaje: "insercion exitosa" });
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+// app.post("/crearempresa", (req, res, next) => {
+//   var registro = req.body;
+//   var instrucciona =
+//     "Insert into Empresa (Empresa) values ('" + registro.empresax + "')";
+//   console.log(instrucciona);
+//   //Voy a insertar lo que me mandaron por parámetro
+//   connection
+//     .execute(instrucciona)
+//     .then((data) => {
+//       console.log(JSON.stringify(data, null, 2));
+//       res.json({ mensaje: "insercion exitosa" });
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.get("/Movimientos/:ID_Empresa", (req, res, next) => {
-  //let instruccion= "SELECT * FROM Articulos";
-  let instruccion =
-    "SELECT m.*, a.Nombre_Articulo FROM Movimientos m INNER JOIN Articulos a on (a.ID_Articulo=m.Articulo) ";
-  //instruccion += " a.empresa FROM Movimientos m INNER JOIN Empresa a on (a.ID_Empresa=m.Empresa) ";
-  if (req.params.ID_Empresa != "0") {
-    instruccion = instruccion + " where a.Empresa= " + req.params.ID_Empresa;
-  }
-  instruccion = instruccion + " order by fecha desc";
+// app.get("/Movimientos/:ID_Empresa", (req, res, next) => {
+//   //let instruccion= "SELECT * FROM Articulos";
+//   let instruccion =
+//     "SELECT m.*, a.Nombre_Articulo FROM Movimientos m INNER JOIN Articulos a on (a.ID_Articulo=m.Articulo) ";
+//   //instruccion += " a.empresa FROM Movimientos m INNER JOIN Empresa a on (a.ID_Empresa=m.Empresa) ";
+//   if (req.params.ID_Empresa != "0") {
+//     instruccion = instruccion + " where a.Empresa= " + req.params.ID_Empresa;
+//   }
+//   instruccion = instruccion + " order by fecha desc";
 
-  connection
-    .query(instruccion)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+//   connection
+//     .query(instruccion)
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.post("/crearremitos", (req, res, next) => {
-  var registro = req.body;
-  var instruccion =
-    "Insert into Movimientos (Tipo, Articulo, empresa, cantidad, Fecha) values (" +
-    registro.tipo +
-    "," +
-    registro.articulo +
-    "," +
-    registro.empresa +
-    "," +
-    registro.cantidad +
-    ",'" +
-    registro.fecha +
-    "')";
-  console.log(instruccion);
-  //Voy a insertar lo que me mandaron por parámetro
-  connection
-    .execute(instruccion)
-    .then((data) => {
-      console.log(JSON.stringify(data, null, 2));
-      res.json({ mensaje: "insercion exitosa" });
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+// app.post("/crearremitos", (req, res, next) => {
+//   var registro = req.body;
+//   var instruccion =
+//     "Insert into Movimientos (Tipo, Articulo, empresa, cantidad, Fecha) values (" +
+//     registro.tipo +
+//     "," +
+//     registro.articulo +
+//     "," +
+//     registro.empresa +
+//     "," +
+//     registro.cantidad +
+//     ",'" +
+//     registro.fecha +
+//     "')";
+//   console.log(instruccion);
+//   //Voy a insertar lo que me mandaron por parámetro
+//   connection
+//     .execute(instruccion)
+//     .then((data) => {
+//       console.log(JSON.stringify(data, null, 2));
+//       res.json({ mensaje: "insercion exitosa" });
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.get("/Articulos", (req, res, next) => {
-  connection
-    .query("SELECT * FROM Articulos")
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+// app.get("/Articulos", (req, res, next) => {
+//   connection
+//     .query("SELECT * FROM Articulos")
+//     .then((data) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
-app.post("/creararticulos", (req, res, next) => {
-  var registro = req.body;
-  var instrucciona =
-    "Insert into Articulos (Nombre_Articulo, Empresa, Foto) values ('" +
-    registro.nombrearticulo +
-    "', " +
-    registro.empresay +
-    ", '" +
-    registro.foto +
-    "')";
-  console.log(instrucciona);
-  //Voy a insertar lo que me mandaron por parámetro
-  connection
-    .execute(instrucciona)
-    .then((data) => {
-      console.log(JSON.stringify(data, null, 2));
-      res.json({ mensaje: "insercion exitosa" });
-    })
-    .catch((error) => {
-      res.render(error);
-    });
-});
+// app.post("/creararticulos", (req, res, next) => {
+//   var registro = req.body;
+//   var instrucciona =
+//     "Insert into Articulos (Nombre_Articulo, Empresa, Foto) values ('" +
+//     registro.nombrearticulo +
+//     "', " +
+//     registro.empresay +
+//     ", '" +
+//     registro.foto +
+//     "')";
+//   console.log(instrucciona);
+//   //Voy a insertar lo que me mandaron por parámetro
+//   connection
+//     .execute(instrucciona)
+//     .then((data) => {
+//       console.log(JSON.stringify(data, null, 2));
+//       res.json({ mensaje: "insercion exitosa" });
+//     })
+//     .catch((error) => {
+//       res.render(error);
+//     });
+// });
 
 //Le indico donde estan mis archivos estáticos
 // y el nombre del archivo por default que quiero que muestre:
 // datos.html
 
-app.use("/", express.static("www", { index: "inicio.html" }));
+app.use("/", express.static("www", { index: "index.html" }));
 
 let port = process.env.PORT || 3000;
 
